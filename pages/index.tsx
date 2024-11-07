50964e34a8fcff95dc0516ca8eee6d2b8b008c92
ultrocharge <oliver.elias011@gmail.com>
2024-11-07 19:39:11 +0000

    feat: Completed Counters Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Hero from "../components/home/Hero";
 import About from "../components/home/About";
+import Counters from "../components/home/Counters";
 export default function Home() {
   return (
     <div>
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
       <Hero />
       <About />
+      <Counters />
     </div>
   );
 }
